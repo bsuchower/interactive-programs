bbef40aac51cad6ceeda13380f653ed263dfa252
Becca Suchower <bsuchower@olin.edu>
2020-12-14 23:45:17 -0500

    Most recent code

--- main_code_test2.py
+++ main_code_test2.py
@@ -13,10 +13,15 @@ white = (255, 255, 255)
 screen_width = 500
 screen_height = 500
 
-note_keys = [K_a, K_s, K_d, K_f, K_g, K_h, K_j]
-notes = {}
+class SoundObjects:
+    """
+    This class creates a sound object for each drum sound, using the
+    built-in Sound class in Pygame.
+    """
+    # create Sound objects for the metronome
+    note_keys = [K_a, K_s, K_d, K_f, K_g, K_h, K_j]
+    notes = {}
 
-def load_sounds(notes):
     notes[K_a] = pygame.mixer.Sound("cowbell-808.wav")
     notes[K_s] = pygame.mixer.Sound("hihat-acoustic02.wav")
     notes[K_d] = pygame.mixer.Sound("kick-acoustic02.wav")
@@ -24,12 +29,7 @@ def load_sounds(notes):
     notes[K_g] = pygame.mixer.Sound("ride-acoustic02.wav")
     notes[K_h] = pygame.mixer.Sound("snare-vinyl02.wav")
     notes[K_j] = pygame.mixer.Sound("tom-acoustic02.wav")
-
-class SoundObjects:
-    """
-    This class creates a sound object for each drum sound, using the
-    built-in Sound class in Pygame.
-    """
+    metronome  = pygame.mixer.Sound("perc-808.wav")
 
     @staticmethod
     def play_sound(sound_object):
@@ -50,7 +50,7 @@ class SoundObjects:
             Plays the sound for a specified amount of time.
         """
         sound_object.play()
-        time.sleep(1)
+        time.sleep(0.5)
         sound_object.stop()
 
 
@@ -109,9 +109,11 @@ class Controller:
     """
     This class controls the user input and event handling.
     """
+    def __init__(self):
+        self.currently_playing = []
 
-    @staticmethod
-    def process_events(notes):
+    # @staticmethod
+    def process_events(self, notes, screen):
         """
         This method handles the events. If the event is to quit the game,
         True is returned, and False is returned otherwise. The keys from
@@ -127,26 +129,30 @@ class Controller:
             if event.type == QUIT:
                 return True
             elif event.type == pygame.KEYDOWN:
-                if event.key in note_keys:
-                    notes[event.key].play()
-            elif event.type == KEYUP:
-                if event.key in note_keys:
-                    notes[event.key].fadeout(500)
+                if event.key in self.currently_playing:
+                    SoundObjects.notes[event.key].stop()
+                elif event.key in SoundObjects.note_keys:
+                    self.currently_playing.append(event.key)
+                    SoundObjects.notes[event.key].play(-1)
+                    Display.color_change(screen)
+            #elif event.type == KEYUP:
+                #if event.key in SoundObjects.note_keys:
+                    #SoundObjects.notes[event.key].fadeout(500)
 
         return False
 
-load_sounds(notes)
 
 def main():
     # set up the screen display
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Drum Machine")
 
+    # initialize done to false
     done = False
 
     # create objects
     view = Display()
-    sounds = SoundObjects
+    sounds = SoundObjects()
     controller = Controller()
     
     # display the screen background
@@ -154,10 +160,10 @@ def main():
 
     while not done:
         # play metronome continuously
-        #SoundObjects.play_sound(SoundObjects.metronome, 0.5)
+        sounds.play_sound(sounds.metronome)
 
         # process events
-        done = controller.process_events(notes)
+        done = controller.process_events(sounds.notes, screen)
 
     # exit the window
     pygame.quit()
